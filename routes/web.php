183376ed7ba2debb69c04d95eb2de6872ccd7637
root <root@tony@cybridge.jp>
2024-05-12 23:47:30 +0900

    install reverb

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
+use App\Events\testingEvent;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/test', function () {
+    event(new testingEvent);
+    return "done";
+});
+

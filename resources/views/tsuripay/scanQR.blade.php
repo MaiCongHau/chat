015d8f9a7dae74b7f8276d27d77e2206b7493791
cba_hosa <hosa@cybridge.jp>
2024-05-20 17:58:19 +0000

    update scan QR code
    update scan QR code

--- resources/views/tsuripay/scanQR.blade.php
+++ resources/views/tsuripay/scanQR.blade.php
@@ -16,7 +16,7 @@
             // Handle the result here
             console.log(`Scan result: ${decodedText}`);
             document.getElementById('qr-reader-results').innerText = `Scan result: ${decodedText}`;
-            const myArray = str.split(",");
+            const myArray = ${decodedText}.split(",");
             
             // Redirect to the URL encoded in the QR code
             window.location.href = '/tsuripay?invoiceId=' + myArray[2];
